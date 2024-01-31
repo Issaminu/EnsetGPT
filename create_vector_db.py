b759c5e2cb6acba665779d808db370b77a11d5fb
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-01-31 13:04:52 +0100

    Add create_vector_db.py script

---
+++ create_vector_db.py
@@ -0,0 +1,32 @@
+import os
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.indexes import VectorstoreIndexCreator
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not os.path.exists("processed"):
+    os.mkdir("processed")
+
+loader = DirectoryLoader("text/www.enset-media.ac.ma")
+documents = loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=1024, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+embedding_function = OpenAIEmbeddings(model="text-embedding-3-large", chunk_size=1024)
+
+vector_store = VectorstoreIndexCreator(
+    vectorstore_kwargs={"persist_directory": "persist"}
+).from_loaders([loader])
+
+# query = "What is Khiat?"
+# db = vector_store.vectorstore.similarity_search(query=query)
+# docs = db.similarity_search(query)
+
+# # print results
+# print(docs[0].page_content)
